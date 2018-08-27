6cf9e74f5836439be926313b109e47a834487ea8
Tom Li <fake@email.com>
2018-08-28 00:15:19 +0800

    Added compass process

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require("gulp"),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
     concat = require('gulp-concat');
 
 //gulp.task('log',function() {
@@ -14,10 +15,12 @@ var jsSources = [
     'components/scripts/tabline.js',
     'components/scripts/template.js'
 ];
+var sassSources = ['components/sass/style.scss'];
+
 gulp.task("coffee",function(){
     gulp.src(coffeeSources)
         .pipe(coffee({bare:true})
-             .on('error',gutil.log))
+        .on('error',gutil.log))
         .pipe(gulp.dest('components/scripts'))
 });
 
@@ -26,5 +29,18 @@ gulp.task('js',function(){
         .pipe(concat('script.js'))
         .pipe(browserify())
         .pipe(gulp.dest('builds/development/js'))
+
+});
+
+
+gulp.task('compass',function(){
+    gulp.src(sassSources)
+        .pipe(compass({
+            sass:'components/sass',
+            image:'build/development/images',
+            style:'expanded'
+        })
+        .on('error',gutil.log))
+        .pipe(gulp.dest('builds/development/css'))
 
 });
