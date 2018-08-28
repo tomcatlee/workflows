a5d713b8f7c25b43435787d6f1547f81acd8af68
Tom Li <fake@email.com>
2018-08-28 15:02:47 +0800

    Minified our html files

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gulp = require("gulp"),
     connect = require('gulp-connect'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
+    minifyHTML = require('gulp-minify-html'),
     concat = require('gulp-concat');
 var env,
     coffeeSources,
@@ -73,12 +74,14 @@ gulp.task('watch',function(){
     gulp.watch(coffeeSources,['coffee']);
     gulp.watch(jsSources,['js']);
     gulp.watch('components/sass/*.scss',['compass']);
-    gulp.watch(htmlSources,['html']);
+    gulp.watch('builds/development/*.html',['html']);
     gulp.watch(jsonSources,['json']);
 });
 
 gulp.task('html',function(){
-    gulp.src(htmlSources)
+    gulp.src('builds/development/*.html')
+    .pipe(gulpif(env==='production',minifyHTML()))
+    .pipe(gulpif(env==='production',gulp.dest(outputDir)))
     .pipe(connect.reload())
 });
 gulp.task('json',function(){
